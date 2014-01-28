a6490c73ddeefc95764e5b807b99b0f97ca00a68
wh1pch81n <wh1pch81n@gmail.com>
2014-01-28 13:07:58 -0800

    Using a custom keyboard of zero height so that we may have the cursor. Setting the displayTextField as firstresponder Added DHTextField subclass. Added custom keyboard textview.  DHBasickeyboardView. zero height

--- TellerCalculator/DHCalculatorBasicViewController.m
+++ TellerCalculator/DHCalculatorBasicViewController.m
@@ -15,6 +15,8 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
+	self.displayTextField.inputView = [[[NSBundle mainBundle] loadNibNamed:@"DHBasicKeyboardView" owner:self options:nil] lastObject];
+	
 	//TODO add KVO here so that when the tabbar's value changes it can change the display's text right away.  For now update the display by hand in the viewdidAppear
 }
 
@@ -22,6 +24,7 @@
 	[super viewDidAppear:animated];
 	DHTabBarController *tabBarController = (DHTabBarController *)self.tabBarController;
 	[self.displayTextField setText:tabBarController.historyModel.historyString];
+	[self.displayTextField becomeFirstResponder];
 }
 
 @end
